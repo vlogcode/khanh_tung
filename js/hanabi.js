40e13f5479b37267614663d13e59b38c04b0d6ba
khanhtung05032001 <sontung130701@gmail.com>
2022-03-12 02:52:13 +0700

    first commit

---
+++ js/hanabi.js
@@ -0,0 +1,13 @@
+$(".gui").click(function (){
+    $(".popup").show();
+    $(".trang-chu-3").hide();
+})
+
+$(".chon4").click(function (){
+    $(".pupp4").show();
+    $(".popup").hide();
+
+})
+$("ipnhap").keypress(function (){
+    $(this).text("tung");
+})
